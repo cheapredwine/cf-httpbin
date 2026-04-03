68ff5cccf1018b1b80983e3cd58a9d12d161880c
Jason Sherron <jsherron@cloudflare.com>
2026-04-02 23:43:05 -0700

    feat: make base64 link clickable with example

--- src/static/content.ts
+++ src/static/content.ts
@@ -258,7 +258,7 @@ export const INDEX_HTML = `<!DOCTYPE html>
   <table>
     <tr><th>Endpoint</th><th>Description</th></tr>
     <tr><td><a href="/uuid">/uuid</a></td><td>Returns UUIDv4</td></tr>
-    <tr><td>/base64/:value</td><td>Decodes base64 value</td></tr>
+    <tr><td><a href="/base64/aGVsbG8=">/base64/:value</a></td><td>Decodes base64 value</td></tr>
     <tr><td><a href="/bytes/1024">/bytes/:n</a></td><td>Returns n random bytes (max 100KB)</td></tr>
   </table>
 
